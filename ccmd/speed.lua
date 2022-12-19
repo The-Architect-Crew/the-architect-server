6fc39f7cda1d26223383dd36ed86e70b0f4d8247
neinwhal <3191240+neinwhal@users.noreply.github.com>
2022-12-20 02:16:27 +0800

    Add speed limits

--- ccmd/speed.lua
+++ ccmd/speed.lua
@@ -6,7 +6,14 @@ minetest.register_chatcommand("speed", {
         if not tonumber(param) then
             minetest.chat_send_player(name, minetest.colorize("grey", "[speed]").." Speed must be a number!")
         else
-		    minetest.get_player_by_name(name):set_physics_override({speed=param})
+            local speed = tonumber(param)
+            if speed <= 0 then
+                speed = 1
+            elseif speed > 100 then
+                speed = 100
+            end
+            minetest.chat_send_player(name, minetest.colorize("grey", "[speed]").." Player speed set to "..speed..".")
+		    minetest.get_player_by_name(name):set_physics_override({speed=speed})
         end
 	end,
 })
